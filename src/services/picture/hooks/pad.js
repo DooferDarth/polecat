c6878cd0dcf4c60b76593e03d09ac74d5ab3c196
Christopher Robert Philabaum <christopher@philabaum.me>
2019-02-23 23:20:53 -0700

    Switched from using lwip to sharp for image processing. (#14)
    Lwip no longer compiles correctly on modern systems and doesn't seem to be maintained any longer.

--- src/services/picture/hooks/pad.js
+++ src/services/picture/hooks/pad.js
@@ -5,7 +5,7 @@
 // Use this hook to manipulate incoming or outgoing data.
 // For more information on hooks see: http://docs.feathersjs.com/hooks/readme.html
 
-const resizer = require('lwip');
+const sharp = require('sharp');
 
 // const defaults = {};
 const encodings = {
@@ -22,27 +22,51 @@ module.exports = test => {
             resolve(hook);
         }
 
-        resizer.open(hook.data.buffer, encodings[hook.data.mimetype], (err, image) => {
-            let batch = image.batch();
-            if(image.width() !== image.height()) {
-                let difference = Math.abs(image.width() - image.height());
-                if(image.width() > image.height()) {
-                    batch.pad(0, Math.ceil(difference / 2), 0, Math.floor(difference / 2), [255, 255, 255, 0]);
-                }
-                else {
-                    batch.pad(Math.floor(difference / 2), 0, Math.ceil(difference / 2), 0, [255, 255, 255, 0]);
-                }
-            }
-            batch.toBuffer(encodings[hook.data.mimetype], (err, buffer) => {
-                if (err) {
-                    reject(err);
-                }
-                else {
-                    hook.data.buffer = buffer;
+        const image = sharp(hook.data.buffer);
 
+        image.metadata((err, metadata) => {
+            if (err) {
+                reject(err);
+            }
+            else {
+                if(metadata.width !== metadata.height) {
+                    let difference = Math.abs(metadata.width - metadata.height);
+
+                    let options = {
+                        top: 0,
+                        bottom: 0,
+                        left: 0,
+                        right: 0,
+                        background: { r: 255, g: 255, b: 255, alpha: 0 }
+                    };
+
+                    if(metadata.width > metadata.height) {
+                        options[left] = Math.ceil(difference / 2);
+                        options[right] = Math.floor(difference / 2);
+                    }
+                    else {
+                        options[top] = Math.ceil(difference / 2);
+                        options[bottom] = Math.floor(difference / 2);
+                    }
+
+                    image
+                        .extend(options)
+                        .toFormat(encodings[hook.data.mimetype])
+                        .toBuffer((err, buffer, info) => {
+                            if (err) {
+                                reject(err);
+                            }
+                            else {
+                                hook.data.buffer = buffer;
+            
+                                resolve(hook);
+                            }
+                        });
+                }
+                else {
                     resolve(hook);
                 }
-            });
-        })
+            }
+        });  
     });
 };
