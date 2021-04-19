a36430b0be93201eb7d6ae16aa15921f5cc89319
Christopher Philabaum <christopher@philabaum.me>
2021-04-18 19:57:23 -0700

    Replace message hooks with feathers-hooks-common

--- src/services/messages/messages.hooks.ts
+++ src/services/messages/messages.hooks.ts
@@ -1,23 +1,61 @@
 import * as authentication from '@feathersjs/authentication';
-import processMessage from '../../hooks/process-message';
-import populateUser from '../../hooks/populate-user';
+import {
+    alterItems,
+    disallow,
+    iff,
+    isProvider,
+    keep,
+    populate,
+    preventChanges,
+    required,
+    setNow,
+    stashBefore
+} from 'feathers-hooks-common';
 // Don't remove this comment. It's needed to format import lines nicely.
 
 const { authenticate } = authentication.hooks;
 
+const userSchema = {
+    include: {
+        service: 'users',
+        nameAs: 'user',
+        parentField: 'userId',
+        childField: '_id'
+    }
+};
+
 export default {
     before: {
-        all: [ authenticate('jwt') ],
+        all: [
+            authenticate('jwt'),
+            required('text'),
+            iff(context => context.params.query?.text.length > 10000, disallow()),
+        ],
         find: [],
         get: [],
-        create: [ processMessage() ],
-        update: [],
-        patch: [],
-        remove: []
+        create: [
+            iff(isProvider('external'), keep('text')),
+            setNow('createdAt', 'updatedAt')
+        ],
+        update: [
+            disallow('external')
+        ],
+        patch: [
+            stashBefore('msg'),
+            iff(context => context.params.user?._id != context.params.msg.user?._id, disallow()),
+            iff(isProvider('external'), keep('text')),
+            setNow('updatedAt')
+        ],
+        remove: [
+            stashBefore('msg'),
+            iff(context => context.params.user?._id != context.params.msg.user?._id, disallow())
+        ]
     },
 
     after: {
-        all: [ populateUser() ],
+        all: [
+            populate({schema: userSchema})
+        ],
         find: [],
         get: [],
         create: [],
